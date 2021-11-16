8e14d889a1acce1a116e356497a43810127857f4
pnarasim <priya.narasimhan@gmail.com>
2021-11-16 17:14:33 +0800

    security fix

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ config.web.port = process.env.WEB_PORT || 8080;
 
 config.web.baseUrl = `http://localhost:${config.web.port}/files/`;
 
-config.db.user = `demo`;
-config.db.password = `demo`; // set up calls to encrypt this with some library and access with decrypts etc
-config.db.connectionString = `mongodb+srv://${config.db.user}:${config.db.password}@cluster0.ghbeq.mongodb.net/test?retryWrites=true&w=majority`;
+config.db.user = ``;
+config.db.password = ``; // set up calls to encrypt this with some library and access with decrypts etc
+config.db.connectionString = `mongodb+srv://${config.db.user}:${config.db.password}@${mycluster}.mongodb.net/test?retryWrites=true&w=majority`;
 config.db.database = "test";
 config.db.imgBucket = "images";
 
